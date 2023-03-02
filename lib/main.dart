dc044892e351f0e9d7dbe9e74f084a33f7168926
dharmishtha <114208599+dharmishthamakwana@users.noreply.github.com>
2023-03-02 20:16:10 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
-import 'package:expanded/page4.dart';
 import 'package:flutter/material.dart';
+import 'package:untitled4/page1.dart';
+import 'package:untitled4/page2.dart';
+import 'package:untitled4/page3.dart';
+import 'package:untitled4/page4.dart';
 void main()
 {
   runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
-        home:Page4(),
+       routes:{
+          '/':(context) => Page1(),
+          'se':(context) => Page2(),
+         'th':(context) => Page3(),
+         'fo':(context) => Page4(),
+       } ,
       )
   );
 }
