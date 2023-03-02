c33f4a50d51ffcd7196f09a0d7763e067084ca79
dharmishtha <114208599+dharmishthamakwana@users.noreply.github.com>
2023-03-02 20:29:39 +0530

    Update page3.dart

--- lib/page3.dart
+++ lib/page3.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+                      import 'package:flutter/material.dart';
 
 // import 'package:pr1/page3.dart';
 // import 'package:pr1/Page2.dart';
@@ -14,182 +14,192 @@ class _Page3State extends State<Page3> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Expanded(
-                              child: Container(
-                                width: 75,
-                                height: 75,
-                                color: Colors.cyan.shade700,
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Expanded(
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  width: 75,
+                                  height: 75,
+                                  color: Colors.cyan.shade700,
+                                ),
                               ),
-                            ),
-                            Expanded(
-                              child: Container(
-                                width: 75,
-                                height: 75,
-                                color: Colors.cyan.shade300,
+                              Expanded(
+                                child: Container(
+                                  width: 75,
+                                  height: 75,
+                                  color: Colors.cyan.shade300,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          width: 150,
-                          height: 75,
-                          color: Colors.cyan.shade500,
+                        Expanded(
+                          child: Container(
+                            width: 150,
+                            height: 75,
+                            color: Colors.cyan.shade500,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Container(
-                    width: 150,
-                    height: 150,
-                    color: Colors.cyan.shade200,
+                  Expanded(
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      color: Colors.cyan.shade200,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: Container(
-                    width: 150,
-                    height: 150,
-                    color: Colors.cyan.shade200,
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      color: Colors.cyan.shade200,
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                        child: Container(
-                          width: 150,
-                          height: 75,
-                          color: Colors.cyan.shade500,
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            width: 150,
+                            height: 75,
+                            color: Colors.cyan.shade500,
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Expanded(
-                              child: Container(
-                                width: 75,
-                                height: 75,
-                                color: Colors.cyan.shade700,
+                        Expanded(
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  width: 75,
+                                  height: 75,
+                                  color: Colors.cyan.shade700,
+                                ),
                               ),
-                            ),
-                            Expanded(
-                              child: Container(
-                                width: 75,
-                                height: 75,
-                                color: Colors.cyan.shade300,
+                              Expanded(
+                                child: Container(
+                                  width: 75,
+                                  height: 75,
+                                  color: Colors.cyan.shade300,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Expanded(
-                              child: Container(
-                                width: 75,
-                                height: 75,
-                                color: Colors.cyan.shade700,
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Expanded(
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  width: 75,
+                                  height: 75,
+                                  color: Colors.cyan.shade700,
+                                ),
                               ),
-                            ),
-                            Expanded(
-                              child: Container(
-                                width: 75,
-                                height: 75,
-                                color: Colors.cyan.shade300,
+                              Expanded(
+                                child: Container(
+                                  width: 75,
+                                  height: 75,
+                                  color: Colors.cyan.shade300,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          width: 150,
-                          height: 75,
-                          color: Colors.cyan.shade500,
+                        Expanded(
+                          child: Container(
+                            width: 150,
+                            height: 75,
+                            color: Colors.cyan.shade500,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: Container(
-                    width: 150,
-                    height: 150,
-                    color: Colors.cyan.shade200,
+                  Expanded(
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      color: Colors.cyan.shade200,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 200),
-            child: Row(
-              children: [
-                InkWell(
-                  onTap: () {
-                    setState(() {
-                      Navigator.pushNamed(context, 'se');
-                    });
-                  },
-                  child: Icon(
-                    Icons.arrow_back,
-                  ),
-                ),
-                InkWell(
+            Padding(
+              padding: const EdgeInsets.only(left: 200),
+              child: Row(
+                children: [
+                  InkWell(
                     onTap: () {
                       setState(() {
-                        Navigator.pushNamed(context, 'fo');
+                        Navigator.pushNamed(context, 'se');
                       });
                     },
-                    child: Icon(Icons.arrow_forward_rounded)),
-              ],
+                    child: Icon(
+                      Icons.arrow_back,
+                      size: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      setState(
+                        () {
+                          Navigator.pushNamed(context, 'fo');
+                        },
+                      );
+                    },
+                    child: Icon(
+                      Icons.arrow_forward_rounded,
+                      size: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
